adcfc158c8c30ca6f5372fdbc4d7fab378b4abed
Justin Lecas <justin.lecas@gmail.com>
2024-02-03 22:52:05 +0100

    init

---
+++ src/main/java/org/laurichapp/passerelle/PasserelleApplication.java
@@ -0,0 +1,13 @@
+package org.laurichapp.passerelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasserelleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasserelleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/laurichapp/passerelle/PasserelleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laurichapp.passerelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasserelleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
